4f1bc54f948f2022d07ad8a1e4aaaae68feafa01
Thada Wangthammang <mildronize@gmail.com>
2020-10-24 15:58:30 +0700

    init project

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './styles.less'
+import { Button } from "antd";
+
+const Package: React.FC = () => (
+  <div className="package">
+    <h2>Do cool stuff</h2>
+    <Button>test</Button>
+  </div>
+)
+
+export default Package
